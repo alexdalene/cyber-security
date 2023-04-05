3407332489376d41fd7a88ce16aaa1f71cea25c8
Alex Dalene <aledal51469@stud.noroff.no>
2023-04-05 13:57:03 +0200

    removed animation for the time being as it didn't work on mobile

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -9,7 +9,7 @@ export default function Hero({title, children}: {title: string, children: React.
     })
 
     return (
-        <motion.header className={styles.header} initial={{ height: '100lvh' }} animate={{ height: '60lvh'}} transition={{ delay: 1, duration: 0.6 }} style={{ y: y }}>
+        <motion.header className={styles.header} initial={{ height: '100lvh' }} transition={{ delay: 1, duration: 0.6 }} style={{ y: y }}>
             <motion.h1 className={styles.display} initial={{ opacity: 0 }} animate={{ opacity: 1, zIndex: 2 }} transition={{ delay: 0.3 }} >{title ? title: 'Default title'}</motion.h1>
             { children }
         </motion.header>
