8953ae1d62bcccfe4058186162195794f39ec998
Alex Dalene <aledal51469@stud.noroff.no>
2023-04-05 13:53:26 +0200

    changed title on hero section

--- app/components/categories.tsx
+++ app/components/categories.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 export default function Categories() {
     return (
         <section className={styles.container}>
-            <motion.h2 className={styles.title} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.4}}>Learn about the various dangers.</motion.h2>
+            <motion.h2 className={styles.title} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.4}}>Learn about the various dangers of the web.</motion.h2>
             <motion.div className={styles.buttons} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5}}>
                 <Link href='/categories/phishing' className={styles.button}>Phishing</Link>
                 <Link href='/categories/privacy-threat' className={styles.button}>Privacy Threat</Link>
